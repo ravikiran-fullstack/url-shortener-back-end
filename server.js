9cd363b80c2fb3ca0fd1a943a3766123a0d12760
kiran-code <ravikiransjce.code@gmail.com>
2021-01-02 13:56:32 +0530

    Added cors to accept request from everywhere

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const ShortUrl = require('./models/shortUrls');
 const generateURLId = require('./utils');
 
 const app = express();
+
+const corsOptions = {
+  // origin : 'http://127.0.0.1:5500'
+  origin : '*'
+}
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 const mongoURI = "mongodb+srv://ravi:test123@urlshortener.6jhak.mongodb.net/urldb?retryWrites=true&w=majority";
@@ -24,6 +31,7 @@ connectToMongoDb();
 
 // Route to post new url to be shortened 
 app.post('/url', (req,res) => {
+  console.log('req.ip',req.ip);
   if(req.body.url === undefined || req.body.url === ''){
     res.status(400).json({message: "Url is undefined or empty"});
   } else {
@@ -39,20 +47,23 @@ app.post('/url', (req,res) => {
 
 // Route to show home page 
 app.get('/', (req, res) => {
+  console.log('req.ip',req.ip);
   res.json({message: 'working'});
 });
 
 // Route to show last few shortened urls along with original url and visit count
 app.get('/recent', (req, res) => {
+  console.log('req.ip',req.ip);
   ShortUrl.find().limit(5).sort({createdAt: 'desc'})
                     .then(result => {
-                      console.log(result);
+                      //console.log(result);
                       res.json(result);
                     }).catch(err => console.log(err));
 });
 
 // Route to search for original url when shortened url is passed and also to update the visitCount 
 app.get('/:shortUrl', (req, res) => {
+  console.log('req.ip',req.ip);
   const shortURLParam = req.params.shortUrl;
   ShortUrl.find({shortUrl: shortURLParam})
                             .then(async result => {
