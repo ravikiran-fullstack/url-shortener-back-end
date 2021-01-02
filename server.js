b0496df4af00ae83c12025538f3948b909d5f023
kiran-code <ravikiransjce.code@gmail.com>
2021-01-02 20:28:03 +0530

    Uploading formatted code

--- server.js
+++ server.js
@@ -1,87 +1,102 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const ShortUrl = require('./models/shortUrls');
-const generateURLId = require('./utils');
+const ShortUrl = require("./models/shortUrls");
+const generateURLId = require("./utils");
 
 const app = express();
 
 const corsOptions = {
   // origin : 'http://127.0.0.1:5500'
-  origin : '*'
-}
+  origin: "*",
+};
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-const mongoURI = "mongodb+srv://ravi:test123@urlshortener.6jhak.mongodb.net/urldb?retryWrites=true&w=majority";
+const mongoURI ="mongodb+srv://ravi:test123@urlshortener.6jhak.mongodb.net/urldb?retryWrites=true&w=majority";
 
 const connectToMongoDb = async () => {
-  try{
-    const result = await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-    app.listen(process.env.PORT || 8585, '0.0.0.0');
-    console.log('Connected to MongoDB');
-  } catch(err) {
+  try {
+    const result = await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(process.env.PORT || 8585, "0.0.0.0");
+    console.log("Connected to MongoDB");
+  } catch (err) {
     console.log(err);
   }
-}
+};
 
 connectToMongoDb();
 
-// Route to post new url to be shortened 
-app.post('/url', (req,res) => {
-  console.log('req.ip',req.ip);
-  if(req.body.url === undefined || req.body.url === ''){
-    res.status(400).json({message: "Url is undefined or empty"});
+// Route to post new url to be shortened
+app.post("/url", (req, res) => {
+  console.log("req.ip", req.ip);
+  if (req.body.url === undefined || req.body.url === "") {
+    res.status(400).json({ message: "Url is undefined or empty" });
   } else {
-    const shortUrl = new ShortUrl({ url: req.body.url,shortUrl: generateURLId(),visitCount: 0});
-    shortUrl.save()
-              .then(result => {
-                res.send(result);
-              }).catch(err => {
-                console.log(err)
-              });
+    const shortUrl = new ShortUrl({
+      url: req.body.url,
+      shortUrl: generateURLId(),
+      visitCount: 0,
+    });
+    shortUrl
+      .save()
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 });
 
-// Route to show home page 
-app.get('/', (req, res) => {
-  console.log('req.ip',req.ip);
-  res.json({message: 'working'});
+// Route to show home page
+app.get("/", (req, res) => {
+  console.log("req.ip", req.ip);
+  res.json({ message: "working" });
 });
 
-app.get('/favicon.ico', (req, res) => {
+app.get("/favicon.ico", (req, res) => {
   res.send();
-})
+});
 
 // Route to show last few shortened urls along with original url and visit count
-app.get('/recent', (req, res) => {
-  console.log('req.ip /recent',req.ip);
-  ShortUrl.find().limit(5).sort({createdAt: 'desc'})
-                    .then(result => {
-                      //console.log(result);
-                      res.json(result);
-                    }).catch(err => console.log(err));
+app.get("/recent", (req, res) => {
+  console.log("req.ip /recent", req.ip);
+  ShortUrl.find()
+    .limit(5)
+    .sort({ createdAt: "desc" })
+    .then((result) => {
+      //console.log(result);
+      res.json(result);
+    })
+    .catch((err) => console.log(err));
 });
 
-// Route to search for original url when shortened url is passed and also to update the visitCount 
-app.get('/:shortUrl', (req, res) => {
-  
-  console.log('req.ip /shortUrl',req.ip);
+// Route to search for original url when shortened url is passed and also to update the visitCount
+app.get("/:shortUrl", (req, res) => {
+  console.log("req.ip /shortUrl", req.ip);
   const shortURLParam = req.params.shortUrl;
 
-  ShortUrl.find({shortUrl: shortURLParam})
-                            .then(async result => {
-                              if(result.length > 0){
-                                res.json(result[0].url);
-                                let visitCount = result[0].visitCount + 1;
-                                await ShortUrl.updateOne({shortUrl: shortURLParam}, {visitCount: visitCount});
-                              } else {
-                                throw new Error('URL not stored in the db');
-                              }
-                            }).catch(err => {
-                              console.log(err);
-                              res.json({message: 'url not found'})
-                            });
-})
+  ShortUrl.find({ shortUrl: shortURLParam })
+    .then(async (result) => {
+      if (result.length > 0) {
+        res.json(result[0].url);
+        let visitCount = result[0].visitCount + 1;
+        await ShortUrl.updateOne(
+          { shortUrl: shortURLParam },
+          { visitCount: visitCount }
+        );
+      } else {
+        throw new Error("URL not stored in the db");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ message: "url not found" });
+    });
+});
